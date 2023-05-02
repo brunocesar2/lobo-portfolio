46c2b46b8364e9ffb32f05dca049921968232450
Bruno César <bruuno.lima00@gmail.com>
2023-05-02 16:02:54 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function scrollToSection() {
+  const internalLinks = document.querySelectorAll(
+    ".js-header-menu a[href^='#']"
+  );
+  if (internalLinks.length) {
+    function scrollToSection(event) {
+      event.preventDefault();
+      const href = event.currentTarget.getAttribute("href");
+      const section = document.querySelector(href);
+
+      section.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    }
+    internalLinks.forEach((link) => {
+      link.addEventListener("click", scrollToSection);
+    });
+  }
+}
