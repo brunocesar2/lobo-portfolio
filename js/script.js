e4ae2727cd1e84e6100c700d53f30590e73d265f
Bruno César <bruuno.lima00@gmail.com>
2023-06-01 00:40:47 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,21 +1,2 @@
-function initScrollToSection() {
-  const internalLinks = document.querySelectorAll(
-    ".js-header-menu a[href^='#']"
-  );
-  if (internalLinks.length) {
-    function scrollToSection(event) {
-      event.preventDefault();
-      const href = event.currentTarget.getAttribute("href");
-      const section = document.querySelector(href);
-
-      section.scrollIntoView({
-        behavior: "smooth",
-        block: "start",
-      });
-    }
-    internalLinks.forEach((link) => {
-      link.addEventListener("click", scrollToSection);
-    });
-  }
-}
+import initScrollToSection from "./modules/scroll-to-section.js";
 initScrollToSection();
