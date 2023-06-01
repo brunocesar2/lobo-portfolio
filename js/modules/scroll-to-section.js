252f299531f58ce18b7a05d77c0fac0f49cd5563
Bruno César <bruuno.lima00@gmail.com>
2023-06-01 16:48:14 -0300

    Update scroll-to-section.js

--- js/modules/scroll-to-section.js
+++ js/modules/scroll-to-section.js
@@ -1,13 +1,14 @@
 export default function initSmoothScroll() {
   const links = document.querySelectorAll("[data-header='menu'] a");
-  if (links.length) {
-    links.forEach((link) => {
-      const href = link.getAttribute("href");
+  links.forEach((link) => {
+    link.addEventListener("click", (event) => {
+      event.preventDefault();
+      const href = event.currentTarget.getAttribute("href");
       const section = document.querySelector(href);
       section.scrollIntoView({
         behavior: "smooth",
         block: "start",
       });
     });
-  }
+  });
 }
