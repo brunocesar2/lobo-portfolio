a1b6efc63ba5da176d969fba1fb764dcbe3f3830
Bruno César <bruuno.lima00@gmail.com>
2023-06-01 17:13:43 -0300

    Update scroll-to-section.js

--- js/modules/scroll-to-section.js
+++ js/modules/scroll-to-section.js
@@ -1,14 +1,15 @@
 export default function initSmoothScroll() {
   const links = document.querySelectorAll("[data-header='menu'] a");
-  links.forEach((link) => {
-    link.addEventListener("click", (event) => {
-      event.preventDefault();
-      const href = event.currentTarget.getAttribute("href");
-      const section = document.querySelector(href);
-      section.scrollIntoView({
-        behavior: "smooth",
-        block: "start",
-      });
+  function smoothScroll(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
     });
+  }
+  links.forEach((link) => {
+    link.addEventListener("click", smoothScroll);
   });
 }
