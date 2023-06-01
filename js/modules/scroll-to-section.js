bb037684ab19dc3d86400e3811a6645fb082fa3c
Bruno César <bruuno.lima00@gmail.com>
2023-06-01 01:45:08 -0300

    Atualizar o scroll-to-section.js

--- js/modules/scroll-to-section.js
+++ js/modules/scroll-to-section.js
@@ -1,14 +1,17 @@
 export default function initScrollToSection() {
   const internalLinks = document.querySelectorAll("[data-header='menu'] a");
   if (internalLinks.length) {
-    internalLinks.forEach((menu) => {
-      menu.addEventListener("click", (event) => {
-        event.preventDefault();
-        const href = event.currentTarget.getAttribute("href");
-        const section = document.querySelector(href);
-        section.scrollIntoView({
-          behavior: "smooth",
-          block: "start",
+  function scrollToSection(event) {
+    event.preventDefault()
+    const href = event.currentTarget.getAttribute("href")
+    const section = document.querySelector(href)
+    section.scrollIntoView ({
+      behavior: "smooth",
+      block: "start",
+    })
+  }
+    internalLinks.forEach(link)
+    link.addEventListener("click", scrollToSection)
         });
       });
     });
