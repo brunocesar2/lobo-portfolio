735c2d52daac202c15b078144d8e75c06e77159a
Bruno <brunocesar.tenorio1@gmail.com>
2023-09-12 14:04:33 -0300

    fix smooth-scroll

--- js/modules/smooth-scroll.js
+++ js/modules/smooth-scroll.js
@@ -1,6 +1,9 @@
 export default function initSmoothScroll() {
   const internalLinks = document.querySelectorAll("[data-smooth-scroll] a");
   if (internalLinks.length) {
+    internalLinks.forEach((links) => {
+      links.addEventListener("click", activeSmoothScroll);
+    });
     function activeSmoothScroll(event) {
       event.preventDefault();
       const href = event.currentTarget.getAttribute("href");
@@ -10,8 +13,5 @@ export default function initSmoothScroll() {
         block: "start",
       });
     }
-    internalLinks.forEach((links) => {
-      links.addEventListener("click", activeSmoothScroll);
-    });
   }
 }
