28989f134771a5302e03dfa93b002996a23c70b3
unknown <brunocesar.tenorio1@gmail.com>
2023-09-30 11:52:48 -0300

    updating script.js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
-import initSmoothScroll from "./modules/smooth-scroll.js";
-initSmoothScroll();
+export default function initSmoothScroll() {
+  const internalLinks = document.querySelectorAll("[data-smooth-scroll] a");
+  if (internalLinks.length) {
+    internalLinks.forEach((links) => {
+      ["touchstart", "click"].forEach((userEvent) => {
+        links.addEventListener(userEvent, activeSmoothScroll);
+      });
+    });
+    function activeSmoothScroll(event) {
+      event.preventDefault();
+      const href = event.currentTarget.getAttribute("href");
+      const section = document.querySelector(href);
+      section.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    }
+  }
+}
