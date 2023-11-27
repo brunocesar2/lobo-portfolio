c8fd6d471346b5d4d429e92eee8de5b10a13da30
Bruno César <brunocesar.tenorio1@gmail.com>
2023-11-27 14:17:12 -0300

    fixing responsive

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 const internalLinks = document.querySelectorAll("[data-smooth-scroll] a");
-  if (internalLinks.length) {
-    internalLinks.forEach((links) => {
-      ["touchstart", "click"].forEach((userEvent) => {
-        links.addEventListener(userEvent, activeSmoothScroll);
-      });
+if (internalLinks.length) {
+  internalLinks.forEach((links) => {
+    ["touchstart", "click"].forEach((userEvent) => {
+      links.addEventListener(userEvent, activeSmoothScroll);
     });
-    function activeSmoothScroll(event) {
-      event.preventDefault();
-      const href = event.currentTarget.getAttribute("href");
-      const section = document.querySelector(href);
-      section.scrollIntoView({
-        behavior: "smooth",
-        block: "start",
-      });
-    }
+  });
+  function activeSmoothScroll(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
   }
+}
